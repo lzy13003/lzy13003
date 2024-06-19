1bcda311ce714ad94fb6d457c8bf0f1861a2f12e
lzy13003 <167947719+lzy13003@users.noreply.github.com>
2024-06-19 10:22:09 +0800

    隨機裁剪

--- box_utils.py
+++ box_utils.py
@@ -120,3 +120,35 @@ def random_expand(img,
     gtboxes[:, 3] = gtboxes[:, 3] / ratio_y
 
     return out_img.astype('uint8'), gtboxes
+
+def multi_box_iou_xywh(box1, box2):
+    """
+    In this case, box1 or box2 can contain multi boxes.
+    Only two cases can be processed in this method:
+       1, box1 and box2 have the same shape, box1.shape == box2.shape
+       2, either box1 or box2 contains only one box, len(box1) == 1 or len(box2) == 1
+    If the shape of box1 and box2 does not match, and both of them contain multi boxes, it will be wrong.
+    """
+    assert box1.shape[-1] == 4, "Box1 shape[-1] should be 4."
+    assert box2.shape[-1] == 4, "Box2 shape[-1] should be 4."
+
+
+    b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
+    b1_y1, b1_y2 = box1[:, 1] - box1[:, 3] / 2, box1[:, 1] + box1[:, 3] / 2
+    b2_x1, b2_x2 = box2[:, 0] - box2[:, 2] / 2, box2[:, 0] + box2[:, 2] / 2
+    b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 3] / 2
+
+    inter_x1 = np.maximum(b1_x1, b2_x1)
+    inter_x2 = np.minimum(b1_x2, b2_x2)
+    inter_y1 = np.maximum(b1_y1, b2_y1)
+    inter_y2 = np.minimum(b1_y2, b2_y2)
+    inter_w = inter_x2 - inter_x1
+    inter_h = inter_y2 - inter_y1
+    inter_w = np.clip(inter_w, a_min=0., a_max=None)
+    inter_h = np.clip(inter_h, a_min=0., a_max=None)
+
+    inter_area = inter_w * inter_h
+    b1_area = (b1_x2 - b1_x1) * (b1_y2 - b1_y1)
+    b2_area = (b2_x2 - b2_x1) * (b2_y2 - b2_y1)
+
+    return inter_area / (b1_area + b2_area - inter_area)
